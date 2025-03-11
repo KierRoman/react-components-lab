8a679cb8acc5766aec3ef5e933fcb6a1e7e63cc2
Kier Roman <kierroman@gmail.com>
2025-03-10 20:33:23 -0500

    WX Updated

---
+++ src/components/WeatherForecast/WeatherIcon/WeatherIcon.jsx
@@ -0,0 +1,8 @@
+// src/components/WeatherIcon/WeatherIcon.jsx
+import React from 'react';
+
+function WeatherIcon({ img, imgAlt }) {
+  return <img src={img} alt={imgAlt} />;
+}
+
+export default WeatherIcon;
